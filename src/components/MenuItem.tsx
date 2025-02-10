2de433535f4368f313518219efcb5c2fad9affa8
Augusto Ana <augustora95@gmail.com>
2025-02-10 12:20:53 -0300

    112. Listando los productos del menú

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,17 @@
+import type { MenuItem } from "../types"
+
 //Instalar: ES7+React/Redux/React-Native snippets. Simple react snippet Video 111. Minuto 0:45
-export default function MenuItem() {
+type MenuItemProps = {
+  item: MenuItem
+}
+
+export default function MenuItem({item} : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button
+    className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+    >
+    <p>{item.name}</p>
+    <p className="font-black">${item.price}</p>
+    </button>
   )
 }
