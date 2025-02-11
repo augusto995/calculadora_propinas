1313aedfd1d3b2ffc982a64572d5ef4bdc78b389
Augusto Ana <augustora95@gmail.com>
2025-02-11 11:06:48 -0300

    113. Creando un Hook para la orden

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import type { OrderItem } from "../types"
+
+export default function useOrder(){
+
+    //Se usa con types mas complejos que typescript no infiere bien
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+    return{
+
+    }
+}
