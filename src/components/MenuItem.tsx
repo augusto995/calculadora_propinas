2cd4b82434bed04b98c643a0a7a860228332d376
Augusto Ana <augustora95@gmail.com>
2025-02-12 16:40:44 -0300

    115. Agregando el signature a la función de agregar articulo con TypeScript

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,14 +3,14 @@ import type { MenuItem } from "../types"
 //Instalar: ES7+React/Redux/React-Native snippets. Simple react snippet Video 111. Minuto 0:45
 type MenuItemProps = {
   item: MenuItem,
-  addItem: () => void
+  addItem: (item: MenuItem) => void
 }
 
 export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button
     className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
-    onClick={() => addItem()}
+    onClick={() => addItem(item)}
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,13 +1,13 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder(){
 
     //Se usa con types mas complejos que typescript no infiere bien
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log('Agregando')
+    const addItem = (item: MenuItem) => {
+        console.log(item)
     }
 
     return{
