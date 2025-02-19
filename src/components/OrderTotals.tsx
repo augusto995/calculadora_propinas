d7a1982220078c7bca0a7e52f485b2c357455e64
Augusto Ana <augustora95@gmail.com>
2025-02-19 19:27:30 -0300

    121. Creando el Componente con los totales

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,25 @@
+
+export default function OrderTotals() {
+  return (
+    <>
+    <div className="space-y-2">
+      <h2 className="font-black text-2xl">Totales y propinas</h2>
+      <p>Subtotal a pagar: {''} 
+        <span className="font-bold">$0</span>
+      </p>
+
+      <p>Propina: {''} 
+        <span className="font-bold">$0</span>
+      </p>
+
+      <p>Total a pagar: {''} 
+        <span className="font-bold">$0</span>
+      </p>
+
+    </div>
+
+    <button></button>
+    
+    </>
+  )
+}
